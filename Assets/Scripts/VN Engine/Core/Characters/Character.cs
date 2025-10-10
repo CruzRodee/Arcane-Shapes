783baf7b8f3fe21d2a520e0db0e7fa4eaf5b0450
rodee <116467683+CruzRodee@users.noreply.github.com>
2025-10-10 08:11:44 +0800

    Implemented Characters talking
    -Characters can talk independent of the dialogue txt files now for external use cases
    -Changed Say functions from void to coroutines

--- Assets/Scripts/VN Engine/Core/Characters/Character.cs
+++ Assets/Scripts/VN Engine/Core/Characters/Character.cs
@@ -1,15 +1,30 @@
 using UnityEngine;
+using DIALOGUE;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace CHARACTERS
 {
     public class Character
     {
         public string name = "";
+        public string displayName = "";
         public RectTransform root = null;
+        public VNDialogueSystem dialogueSystem => VNDialogueSystem.instance;
 
         public Character(string name)
         {
             this.name = name;
+            displayName = name;
+        }
+
+        // External functionality if you don't want to use the dialogue text files to make characters speak
+        public Coroutine Say(string dialogue) => Say(new List<string> { dialogue });
+
+        public Coroutine Say(List<string> dialogue)
+        {
+            dialogueSystem.ShowSpeakerName(displayName);
+            return dialogueSystem.Say(dialogue);
         }
 
         public enum CharacterType
--- Assets/Scripts/VN Engine/Core/_TESTING/TestCharacters.cs
+++ Assets/Scripts/VN Engine/Core/_TESTING/TestCharacters.cs
@@ -10,9 +10,52 @@ namespace TESTING
         // Start is called before the first frame update
         void Start()
         {
+            // Character Oz = CharacterManager.instance.CreateCharacter("Oz");
+            // Character Student = CharacterManager.instance.CreateCharacter("S");
+            // Character Player = CharacterManager.instance.CreateCharacter("Player");
+
+            StartCoroutine(Test());
+        }
+
+        IEnumerator Test()
+        {
+            // List<string> lines = new List<string>()
+            // {
+            //     "Oz \"Hello! This is a test of the Say function.\"",
+            //     "S \"This is another line of dialogue.\"",
+            //     "Player \"And another one!\"",
+            //     "",
+            //     "This line has no speaker, so it should be treated as narration.",
+            //     "Oz \"Finally, this is the last line.\""
+            // };
+
+            // yield return VNDialogueSystem.instance.Say(lines);
+
             Character Oz = CharacterManager.instance.CreateCharacter("Oz");
             Character Student = CharacterManager.instance.CreateCharacter("S");
             Character Player = CharacterManager.instance.CreateCharacter("Player");
+
+            List<string> ozLines = new List<string>()
+            {
+                "Hello! This is a test of the Say function.",
+                "This is the second line of dialogue.",
+                "And this is the third and final line."
+            };
+
+            yield return Oz.Say(ozLines);
+
+            List<string> studentLines = new List<string>()
+            {
+                "Hi Oz!{c} This is Student speaking.",
+                "I'm testing the Say function as well.",
+                "It's. . .{wa 2} working great!"
+            };
+
+            yield return Student.Say(studentLines);
+
+            yield return Player.Say("Hey everyone, Player here!{a} Just wanted to say hi.");
+
+            Debug.Log("Finished");
         }
 
         // Update is called once per frame
