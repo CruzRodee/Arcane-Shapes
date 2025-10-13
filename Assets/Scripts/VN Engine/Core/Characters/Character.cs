250e4edad874135970d9734b59cbdd88c317adaa
rodee <116467683+CruzRodee@users.noreply.github.com>
2025-10-13 18:35:58 +0800

    Implemented Character Movement

--- Assets/Scripts/VN Engine/Core/Characters/Character.cs
+++ Assets/Scripts/VN Engine/Core/Characters/Character.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using DIALOGUE;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -19,8 +18,10 @@ namespace CHARACTERS
 
         // Coroutines
         protected Coroutine co_revealing, co_hiding;
+        protected Coroutine co_moving;
         public bool isRevealing => co_revealing != null;
         public bool isHiding => co_hiding != null;
+        public bool isMoving => co_moving != null;
         public virtual bool isVisible => false;
 
         public Character(string name, CharacterConfigData config, GameObject prefab)
@@ -87,6 +88,70 @@ namespace CHARACTERS
             Debug.LogWarning("Character.ShowingOrHiding() called, but not implemented in derived class. It cannot be called from a base character type.");
             yield return null;
         }
+
+        public virtual void SetPosition(Vector2 position)
+        {
+            if (root == null)
+                return;
+
+            (Vector2 minAnchorTarget, Vector2 maxAnchorTarget) = ConvertUITargetPositionToRelativeCharacterAnchorTargets(position);
+            root.anchorMin = minAnchorTarget;
+            root.anchorMax = maxAnchorTarget;
+        }
+
+        public virtual Coroutine MoveToPosition(Vector2 position, float speed = 2f, bool smooth = false)
+        {
+            if (root == null)
+                return null;
+
+            if (isMoving)
+                manager.StopCoroutine(co_moving);
+
+            co_moving = manager.StartCoroutine(MovingToPosition(position, speed, smooth));
+
+            return co_moving;
+        }
+
+        private IEnumerator MovingToPosition(Vector2 position, float speed, bool smooth)
+        {
+            (Vector2 minAnchorTarget, Vector2 maxAnchorTarget) = ConvertUITargetPositionToRelativeCharacterAnchorTargets(position);
+            Vector2 padding = root.anchorMax - root.anchorMin;
+
+            while (root.anchorMin != minAnchorTarget || root.anchorMax != maxAnchorTarget)
+            {
+                root.anchorMin = smooth ?
+                    Vector2.Lerp(root.anchorMin, minAnchorTarget, Time.deltaTime * speed) :
+                    Vector2.MoveTowards(root.anchorMin, minAnchorTarget, Time.deltaTime * speed * 0.35f);
+
+                root.anchorMax = root.anchorMin + padding;
+
+                if (smooth && Vector2.Distance(root.anchorMin, minAnchorTarget) <= 0.001f)
+                {
+                    root.anchorMin = minAnchorTarget;
+                    root.anchorMax = maxAnchorTarget;
+                    break;
+                }
+
+                yield return null;
+            }
+
+            Debug.Log($"Character {name} finished moving to position {position}.");
+            co_moving = null;
+        }
+
+        protected (Vector2, Vector2) ConvertUITargetPositionToRelativeCharacterAnchorTargets(Vector2 position)
+        {
+            Vector2 padding = root.anchorMax - root.anchorMin;
+
+            float maxX = 1f - padding.x;
+            float maxY = 1f - padding.y;
+
+            Vector2 minAnchorTarget = new Vector2(maxX * position.x, maxY * position.y);
+            Vector2 maxAnchorTarget = minAnchorTarget + padding;
+            return (minAnchorTarget, maxAnchorTarget);
+        }
+
+
 
         public enum CharacterType
         {
--- Assets/Scripts/VN Engine/Core/_TESTING/TestCharacters2.cs
+++ Assets/Scripts/VN Engine/Core/_TESTING/TestCharacters2.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using CHARACTERS;
 using TMPro;
@@ -26,6 +25,7 @@ namespace TESTING
 
         IEnumerator Test()
         {
+            // Basic Test on Show/Hide
             // Character Raelin = CharacterManager.instance.CreateCharacter("Raelin");
 
             // yield return new WaitForSeconds(1f);
@@ -38,22 +38,53 @@ namespace TESTING
 
             // yield return Raelin.Say("Hello there!");
 
-            Character guard1 = CreateCharacter("Guard1 as Generic");
-            Character guard2 = CreateCharacter("Guard2 as Generic");
-            Character guard3 = CreateCharacter("Guard3 as Generic");
 
-            guard1.Show();
-            guard2.Show();
-            guard3.Show();
+            // Guards Test
+            // Character guard1 = CreateCharacter("Guard1 as Generic");
+            // Character guard2 = CreateCharacter("Guard2 as Generic");
+            // Character guard3 = CreateCharacter("Guard3 as Generic");
 
-            guard1.SetDialogueFont(tempFont);
-            guard1.SetNameFont(tempFont);
-            guard2.SetDialogueColor(Color.cyan);
-            guard3.SetNameColor(Color.red);
+            // guard1.SetPosition(Vector2.zero);
+            // guard2.SetPosition(new Vector2(0.5f, 0.5f));
+            // guard3.SetPosition(Vector2.one);
 
-            yield return guard1.Say("Halt! Who goes there?");
-            yield return guard2.Say("State your business!");
-            yield return guard3.Say("You shall not pass!");
+            // guard1.Show();
+            // guard2.Show();
+            // guard3.Show();
+
+            // guard1.SetDialogueFont(tempFont);
+            // guard1.SetNameFont(tempFont);
+            // guard2.SetDialogueColor(Color.cyan);
+            // guard3.SetNameColor(Color.red);
+
+            // yield return guard1.Say("Halt! Who goes there?");
+            // yield return guard2.Say("State your business!");
+            // yield return guard3.Say("You shall not pass!");
+
+            Character Guard = CreateCharacter("Guard1 as Generic");
+            Character Raelin = CreateCharacter("Raelin");
+            Character Student = CreateCharacter("Female Student 2");
+
+            Guard.SetPosition(Vector2.zero);
+            Raelin.SetPosition(new Vector2(0.5f, 0.5f));
+            Student.SetPosition(Vector2.one);
+
+            Raelin.Show();
+            Student.Show();
+
+            yield return Guard.Show();
+            yield return Guard.MoveToPosition(Vector2.one, smooth: true);
+            yield return Guard.MoveToPosition(Vector2.zero, smooth: true);
+
+            Guard.SetDialogueFont(tempFont);
+            Guard.SetNameFont(tempFont);
+            Raelin.SetDialogueColor(Color.cyan);
+            Student.SetNameColor(Color.red);
+
+            yield return Guard.Say("Halt! Who goes there?");
+            yield return Raelin.Say("State your business!");
+            yield return Student.Say("You shall not pass!");
+
 
             Debug.Log("Finished Testing Characters 2");
 
