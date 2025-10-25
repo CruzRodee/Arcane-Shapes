5983284d9bb1b13cc634322ed42838da9631d6b3
rodee <116467683+CruzRodee@users.noreply.github.com>
2025-10-25 16:46:20 +0800

    Implemented Graphic Panel Structure
    - This will allow us to change the background and play videos soon

---
+++ Assets/Scripts/VN Engine/Scripts/Core/_TESTING/GraphicLayerTesting.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using UnityEngine;
+
+public class GraphicLayerTesting : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Running());
+    }
+
+    IEnumerator Running()
+    {
+        GraphicPanel panel = GraphicPanelManager.instance.GetPanel("Background");
+        GraphicLayer layer = panel.GetLayer(0, true);
+
+        yield return new WaitForSeconds(1f);
+
+        layer.SetTexture("Graphics/BG Images/2");
+
+        Texture blendTex = Resources.Load<Texture>("Graphics/Transition Effects/hurricane");
+        layer.SetTexture("Graphics/BG Images/2", blendingTexture: blendTex);
+
+        //layer.currentGraphic.renderer.material.SetColor("_Color", Color.red);
+    }
+
+
+}
