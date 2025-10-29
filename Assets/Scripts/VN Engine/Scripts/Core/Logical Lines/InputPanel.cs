62b6c1207a12ef5ff7f74d742f30ddd50178aa3e
rodee <116467683+CruzRodee@users.noreply.github.com>
2025-10-29 21:21:55 +0800

    Implemented Input Panel
    - Converted legacy input panel stuff into tmp for customization
    - Should properly work in the game just need to be integrated into dialogue files now

---
+++ Assets/Scripts/VN Engine/Scripts/Core/Logical Lines/InputPanel.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class InputPanel : MonoBehaviour
+{
+    [SerializeField] private CanvasGroup canvasGroup;
+    [SerializeField] private TMP_Text titleText;
+    [SerializeField] private Button acceptButton;
+    [SerializeField] private TMP_InputField inputField;
+
+    private CanvasGroupController cg;
+
+    public string lastInput { get; private set; } = string.Empty;
+    public bool isWaitingOnUserInput { get; private set; } = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cg = new CanvasGroupController(this, canvasGroup);
+
+        canvasGroup.alpha = 0;
+        SetCanvasState(false);
+        acceptButton.gameObject.SetActive(false);
+
+        inputField.onValueChanged.AddListener(OnInputChanged);
+        acceptButton.onClick.AddListener(OnAcceptInput);
+    }
+
+
+    public void Show(string title)
+    {
+        titleText.text = title;
+        inputField.text = string.Empty;
+        cg.Show();
+        SetCanvasState(true);
+        isWaitingOnUserInput = true;
+    }
+
+    public void Hide()
+    {
+        cg.Hide();
+        SetCanvasState(false);
+        isWaitingOnUserInput = false;
+    }
+
+    public void OnAcceptInput()
+    {
+        if (inputField.text == string.Empty)
+        {
+            return;
+        }
+
+        lastInput = inputField.text;
+        Hide();
+    }
+
+    private void SetCanvasState(bool active)
+    {
+        canvasGroup.interactable = active;
+        canvasGroup.blocksRaycasts = active;
+    }
+
+    public void OnInputChanged(string value)
+    {
+        acceptButton.gameObject.SetActive(HasValidText());
+    }
+
+    private bool HasValidText()
+    {
+        return inputField.text != string.Empty;
+    }
+
+
+}
